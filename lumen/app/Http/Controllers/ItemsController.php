150d2ccd3412489b7bde96cb5df0381069aa083d
Bruno Heinrich <brunu1994@gmail.com>
2021-02-03 00:24:14 -0300

    get is working, 3 to go

--- lumen/app/Http/Controllers/ItemsController.php
+++ lumen/app/Http/Controllers/ItemsController.php
@@ -1,26 +1,17 @@
 <?php
 namespace App\Http\Controllers;
-
-use App\Item;
+use Illuminate\Support\Facades\DB;
 
 class ItemsController extends Controller {
-    // private $_model = "App\Item";
     use RESTActions;
 
-    public function index($id) {
-        return User::findOrFail($id);
-        // $m = $this->_model;
-        // return $this->respond(Response::HTTP_OK, $m::with('item')->get());
+    public function index() {
+        $items = DB::table('item')
+            ->select()
+            ->get();
+
+        $items = \json_encode($items, true);
+
+        print_r($items);
     }
-
-    // public function show_user_messages($id) {
-    //     $m = $this->_model;
-    //     $model = $m::where('user_id', $id)->get();
-
-    //     if (is_null($model)) {
-    //         return $this->respond(Response::HTTP_NOT_FOUND);
-    //     }
-
-    //     return $this->respond(Response::HTTP_OK, $model);
-    // }
 }
--- lumen/routes/web.php
+++ lumen/routes/web.php
@@ -33,7 +33,7 @@ $router->group(['prefix' => 'messages'], function () use ($router) {
  * Routes for resource users
  */
 $router->group(['prefix' => 'users'], function () use ($router) {
-    $router->get('/{id}', 'UsersController@index');
+    $router->get('/', 'UsersController@index');
     $router->get('/{id}', 'UsersController@show');
     $router->post('/', 'UsersController@store');
     $router->put('/{id}', 'UsersController@update');
@@ -44,9 +44,9 @@ $router->group(['prefix' => 'users'], function () use ($router) {
  * Routes for resource item
  */
 $router->group(['prefix' => 'item'], function () use ($router) {
-    $router->get('item', 'ItemsController@index');
-    $router->get('item/{id}', 'ItemsController@show');
-    $router->post('item', 'ItemsController@store');
-    $router->put('item/{id}', 'ItemsController@update');
-    $router->delete('item/{id}', 'ItemsController@destroy');
+    $router->get('', 'ItemsController@index');
+    $router->get('/{id}', 'ItemsController@show');
+    $router->post('', 'ItemsController@store');
+    $router->put('/{id}', 'ItemsController@update');
+    $router->delete('/{id}', 'ItemsController@destroy');
 });
